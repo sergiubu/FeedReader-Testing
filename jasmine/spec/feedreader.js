abffcb9da03b3d820b3a9b003a82bfe0fdaa3eaf
Sergiu <sil3ent@yahoo.com>
2018-05-10 16:05:23 +0300

    Test: check if feed container has at least one element after loadFeed is done

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -77,15 +77,24 @@ $(function() {
             icon.click();
             expect(body.classList.contains('menu-hidden')).toBe(true);
           });
+    });
     /* TODO: Write a new test suite named "Initial Entries" */
-
+    describe('Initial Entries', function() {
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+         beforeEach(function(done) {
+           loadFeed(0, done);
+         });
 
+         it('has at least one .entry element', function(done) {
+           const feed = document.querySelector('.feed');
+           expect(feed.length).not.toBe(0);
+           done();
+         });
     /* TODO: Write a new test suite named "New Feed Selection" */
 
         /* TODO: Write a test that ensures when a new feed is loaded
